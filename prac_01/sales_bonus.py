5e42052cef3ea3b58e657798a5f25e2ac46a05f3
ChrisPrince99 <Christopher.Prince@my.jcu.edu.au>
2019-08-07 13:45:41 +1000

    ...

--- prac_01/sales_bonus.py
+++ prac_01/sales_bonus.py
@@ -4,15 +4,11 @@ If sales are under $1,000, the user gets a 10% bonus.
 If sales are $1,000 or over, the bonus is 15%.
 """
 sales = float(input("Enter your sales: $"))
-while sales > 0:
+while sales >= 0:
     if sales < 1000:
         bonus = sales * 0.10
-        print(bonus)
-        sales = float(input("Enter your sales: $"))
     else:
         bonus = sales * 0.15
-        print(bonus)
-        sales = float(input("Enter your sales: $"))
-    if sales < 0:
-        print("Thank you and goodbye.")
-        break
+    print(bonus)
+    sales = float(input("Enter your sales: $"))
+print("Thank you and goodbye.")
---
+++ prac_01/shop_calculator.py
@@ -0,0 +1,24 @@
+# A shop requires a small program that would allow them to quickly work
+# out the total price for a number of items, each with different prices.
+#
+# The program allows the user to enter the number of items and the price of each different item.
+# Then the program computes and displays the total price of those items.
+# If the total price is over $100, then a 10% discount is applied
+# to that total before the amount is displayed on the screen.
+number_of_items = int(input("Please enter the number of items to be calculated for: #"))
+while 0 > number_of_items:
+    print("Error: invalid input")
+    number_of_items = int(input("Please enter the number of items to be calculated for: #"))
+total = 0
+total_discount = 0
+for i in range(number_of_items):
+    price_of_item = int(input("Please enter the price of item number " + str(i + 1)))
+    total += price_of_item
+if total > 100:
+    total_discount = total * 0.10
+
+total -= total_discount
+print("Number of Items:", number_of_items)
+for i in range(number_of_items):
+    print("Price of item:", prices[i])
+print("Total price for", number_of_items, "items:", total)
